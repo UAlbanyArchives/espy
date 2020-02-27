90ac85cefb4fac5c2aa2114117725fcd23432863
gwiedeman <gregory.wiedeman1@gmail.com>
2020-02-27 10:47:16 -0500

    sync prod changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+  scope 'espy' do
   mount Blacklight::Engine => '/'
       concern :marc_viewable, Blacklight::Marc::Routes::MarcViewable.new
 
@@ -26,4 +26,5 @@ Rails.application.routes.draw do
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 end
